20d27fd1546bd7d90691eb63c71713f3bdeabeea
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-22 14:27:51 +0100

    herg

--- partie_1_tribulles_03.py
+++ partie_1_tribulles_03.py
@@ -10,3 +10,7 @@ for entier in range(len(myTable)):
             myTable[i-1] = myTable[i]
             myTable[i] = inversion
             print(myTable)
+
+#Je pense que le tri à bulle est considéré comme lent car on doit comparé l'élément le plus grand
+# avec l'entiérété de la liste pour le faire remonté et cela pour chaque numéro de la grille
+# Malgré cela son temps d'exécution est proche d'une milliseconde     
