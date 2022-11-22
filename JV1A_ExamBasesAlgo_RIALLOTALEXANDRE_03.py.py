ff31e466621645c11a6b70fc5652e1b066be5913
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-22 14:57:16 +0100

    Update JV1A_ExamBasesAlgo_RIALLOTALEXANDRE_03.py.py

--- JV1A_ExamBasesAlgo_RIALLOTALEXANDRE_03.py.py
+++ JV1A_ExamBasesAlgo_RIALLOTALEXANDRE_03.py.py
@@ -1,9 +1,21 @@
-myTable = [86,26,31,72,42]
+myTable = [72,26,31,86,42]
 print(myTable)
 grand=myTable[0]
 inversion=0
-for i in range (len(myTable)):
-    
+for tri in range (len(myTable)):
+    #recherche élèment plus grand
+    for recherche in range(len(myTable)):
+        grand=myTable[recherche]
+        if (grand < myTable[recherche]):
+            grand = myTable(recherche)
+
+    for i in range (len(myTable)):
+        if (grand > myTable[i]):
+            grand=myTable[i-1]
+            inversion=myTable[i-1]
+            myTable[i-1] = myTable[i]
+            myTable[i] = inversion
+        print(myTable)
 
 #Je pense que le tri à bulle est considéré comme lent car on doit comparé l'élément le plus grand
 # avec l'entiérété de la liste pour le faire remonté et cela pour chaque numéro de la grille
