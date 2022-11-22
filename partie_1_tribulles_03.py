546f8aa302abe196ea96aa0a10b9fe2b5d92716e
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-22 14:27:14 +0100

    nbgfb

--- partie_1_tribulles_03.py
+++ partie_1_tribulles_03.py
@@ -2,13 +2,11 @@ myTable = [86,26,31,72,42]
 print(myTable)
 grand=myTable[0]
 inversion=0
-positiongrand=0
-for i in range (len(myTable)):
-    for c in range (i,len(myTable)):
-        if (grand>myTable[c]):
-            grand=myTable[c]
-            positiongrand=c
-        inversion=myTable[i]
-        myTable[positiongrand]=inversion
-        myTable[i]=grand
-        print(myTable)
+for entier in range(len(myTable)):
+    for i in range (len(myTable)):
+        if (grand > myTable[i]):
+            grand=myTable[i-1]
+            inversion=myTable[i-1]
+            myTable[i-1] = myTable[i]
+            myTable[i] = inversion
+            print(myTable)
